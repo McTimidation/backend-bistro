7e11440f722f0f4d3ad7e2000da10f73b2621d4d
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-11-09 21:18:04 +0000

    default commit

--- bistro/menu_items/urls.py
+++ bistro/menu_items/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from . import views
 
-# urlpatterns = [
-#     path('menu_items/', views.get_all_menu)
-# ]
+urlpatterns = [
+    path('menu_items/', views.get_all_menus)
+]
--- bistro/menu_items/views.py
+++ bistro/menu_items/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Menu_Item
 
-# def get_all_menus(request):
-#     return JsonResponse(data)
+
+def get_all_menus(request):
+    items = list(Menu_Item.objects.values())
+    print(items)
+    return JsonResponse({'data': items})
 
 
